f9632284233ec5f4383c9d00ceb02f5886140cda
swwind <i@sww.moe>
2023-03-18 23:28:13 +0800

    docs: add cloudflare web analytics

--- packages/docs/src/root.tsx
+++ packages/docs/src/root.tsx
@@ -26,6 +26,13 @@ export default component$(() => {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Inter"
         />
+        {/* Cloudflare Web Analytics */}
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "be12df9271ef4ec59f5161c657985889"}'
+        />
+        {/* End Cloudflare Web Analytics */}
         <RouterHead />
       </head>
       <body lang="en">
